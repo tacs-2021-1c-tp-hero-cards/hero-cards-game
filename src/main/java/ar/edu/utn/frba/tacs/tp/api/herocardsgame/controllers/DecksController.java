46935e1985f7944dc3f92b8fc532dd687df9688a
ahufa <facundo.ahumada@despegar.com>
2021-04-20 08:29:35 -0300

    [deck-model] Add response in DecksController

--- src/main/java/ar/edu/utn/frba/tacs/tp/api/herocardsgame/controllers/DecksController.java
+++ src/main/java/ar/edu/utn/frba/tacs/tp/api/herocardsgame/controllers/DecksController.java
@@ -1,13 +1,18 @@
 package ar.edu.utn.frba.tacs.tp.api.herocardsgame.controllers;
 
+import ar.edu.utn.frba.tacs.tp.api.herocardsgame.models.game.Card;
+import ar.edu.utn.frba.tacs.tp.api.herocardsgame.models.game.Deck;
+import ar.edu.utn.frba.tacs.tp.api.herocardsgame.request.AddCardToDeckRequest;
+import ar.edu.utn.frba.tacs.tp.api.herocardsgame.request.UpdateNameDeckRequest;
+import ar.edu.utn.frba.tacs.tp.api.herocardsgame.utils.FileConstructorUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * El administrador tiene la capacidad de manejar los mazos (crear, eliminar, modificar)
@@ -16,35 +21,59 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class DecksController {
 
+  @GetMapping("/users/decks")
+  public ResponseEntity<List<Deck>> getDecks() {
+    Card batman = FileConstructorUtils.createFromFile("src/test/resources/json/card/Batman.json", Card.class);
+    Card flash = FileConstructorUtils.createFromFile("src/test/resources/json/card/Flash.json", Card.class);
 
-  @GetMapping("/decks")
-  public void getDecks() { }
+    Deck batmanDeck = new Deck(1L, "Batman deck", Collections.singletonList(batman));
+    Deck flashDeck = new Deck(2L, "Justice League deck", Arrays.asList(batman, flash));
+
+    return ResponseEntity.status(HttpStatus.OK).body(Arrays.asList(batmanDeck, flashDeck));
+  }
 
   /**
    *  TODO we should validate all the cards added has attributes needed for the game.
-   * @param deckId
+   * @param deck
+   * @return
    */
   @PostMapping("/admin/decks")
-  @RequestMapping(method = RequestMethod.POST, value = {"/admin/decks/{deckId}"})
-  public void createDeck(@PathVariable("deckId") String deckId) {
-    System.out.println(deckId);
+  public ResponseEntity<Deck> createDeck(@RequestBody Deck deck) {
+    return ResponseEntity.status(HttpStatus.CREATED).body(deck);
   }
 
   /**
    * TODO use the same validation to create the deck
    * @param deckId
+   * @return
    */
-  @PutMapping("/admin/decks")
-  @RequestMapping(method = RequestMethod.PUT, value = {"/admin/decks/{deckId}"})
-  public void updateDeck(@PathVariable("deckId") String deckId) {
-    System.out.println(deckId);
+  @PutMapping("/admin/decks/{deckId}")
+  public ResponseEntity updateDeck(@PathVariable("deckId") String deckId, @RequestBody Deck deck) {
+    return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
   }
 
-  @DeleteMapping("/admin/decks")
-  @RequestMapping(method = RequestMethod.DELETE, value = {"/admin/decks/{deckId}"})
-  public void deleteDeck(@PathVariable("deckId") String deckId) {
-    System.out.println(deckId);
+  @PatchMapping("/admin/decks/{deckId}/name")
+  public ResponseEntity updateNameDeck(@PathVariable("deckId") String deckId,
+                                       @RequestBody UpdateNameDeckRequest updateNameDeckRequest){
+    return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
   }
 
+  //TODO: Tengo dudas con respecto a si es put o patch
+  //TODO: Tengo dudas con respecto a si conviene agregar la carta por id o por objeto
+  @PatchMapping("/admin/decks/{deckId}/card")
+  public ResponseEntity addCardToDeck(@PathVariable("deckId") String deckId,
+                                      @RequestBody AddCardToDeckRequest addCardToDeckRequest){
+    return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+  }
+
+  @DeleteMapping("/admin/decks/{deckId}/card/{cardId}")
+  public ResponseEntity deleteCardToDeck(@PathVariable("deckId") String deckId, @PathVariable("cardId") String cardId){
+    return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+  }
+
+  @DeleteMapping("/admin/decks/{deckId}")
+  public ResponseEntity deleteDeck(@PathVariable("deckId") String deckId) {
+    return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+  }
 
 }
