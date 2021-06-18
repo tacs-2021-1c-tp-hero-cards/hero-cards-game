c3d8650af57415d7faa632c0c0fc709c06b7f406
ahufa <facundo.ahumada@despegar.com>
2021-06-18 19:56:32 -0300

    Si un atributo es invalido dentro de la carta settearle el valor -1

--- src/main/kotlin/ar/edu/utn/frba/tacs/tp/api/herocardsgame/models/game/Card.kt
+++ src/main/kotlin/ar/edu/utn/frba/tacs/tp/api/herocardsgame/models/game/Card.kt
@@ -3,6 +3,8 @@ package ar.edu.utn.frba.tacs.tp.api.herocardsgame.models.game
 import ar.edu.utn.frba.tacs.tp.api.herocardsgame.service.duel.DuelResult
 import ar.edu.utn.frba.tacs.tp.api.herocardsgame.service.duel.DuelStrategy
 import ar.edu.utn.frba.tacs.tp.api.herocardsgame.service.duel.DuelType
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 data class Card(
     val id: Long,
@@ -10,6 +12,25 @@ data class Card(
     val powerstats: Powerstats,
     val imageUrl: String
 ) {
+
+    private val log: Logger = LoggerFactory.getLogger(Card::class.java)
+
     fun duel(otherCard: Card, duelType: DuelType): DuelResult =
         DuelStrategy().getDuelStrategy(duelType).invoke(this, otherCard)
+
+    fun isInvalidPowerstats(): Boolean {
+        val invalidPowers = powerstats.calculateInvalidPowers()
+        val notEmpty = invalidPowers.isNotEmpty()
+
+        if (notEmpty) {
+            val invalidPowersString = invalidPowers.joinToString(
+                prefix = "[",
+                separator = ", ",
+                postfix = "]",
+            )
+            log.warn("Card with id: $id has invalid $invalidPowersString attributes")
+        }
+
+        return notEmpty
+    }
 }
--- src/main/kotlin/ar/edu/utn/frba/tacs/tp/api/herocardsgame/models/game/Powerstats.kt
+++ src/main/kotlin/ar/edu/utn/frba/tacs/tp/api/herocardsgame/models/game/Powerstats.kt
@@ -8,4 +8,18 @@ data class Powerstats(
     val power: Int,
     val combat: Int,
     val strength: Int,
-)
+){
+    fun calculateInvalidPowers(): List<String> {
+        val invalidPowers = mutableListOf<String>()
+
+        if(height == -1) invalidPowers.add("height")
+        if(weight == -1) invalidPowers.add("weight")
+        if(intelligence == -1) invalidPowers.add("intelligence")
+        if(speed == -1) invalidPowers.add("speed")
+        if(power == -1) invalidPowers.add("power")
+        if(combat == -1) invalidPowers.add("combat")
+        if(strength == -1) invalidPowers.add("strength")
+
+        return invalidPowers.toList()
+    }
+}
